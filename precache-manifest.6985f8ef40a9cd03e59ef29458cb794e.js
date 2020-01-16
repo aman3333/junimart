1e2e2e2b37798fc6acb2933d08913030cce7b5ca
Aman Jain <amanjain.3331@gmail.com>
2020-01-17 00:29:30 +0530

    first commit

---
+++ precache-manifest.6985f8ef40a9cd03e59ef29458cb794e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "04fc30656df740608005",
+    "url": "/static/css/main.3ffeab83.chunk.css"
+  },
+  {
+    "revision": "04fc30656df740608005",
+    "url": "/static/js/main.04fc3065.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1d3efda3f675d6895e52",
+    "url": "/static/css/2.ca0a562b.chunk.css"
+  },
+  {
+    "revision": "1d3efda3f675d6895e52",
+    "url": "/static/js/2.1d3efda3.chunk.js"
+  },
+  {
+    "revision": "cdd0a0ced87d091efce25133ec013d0b",
+    "url": "/static/media/uniform.cdd0a0ce.png"
+  },
+  {
+    "revision": "8c63685786dc6b645a76dc4e37c2c008",
+    "url": "/static/media/banner1.8c636857.png"
+  },
+  {
+    "revision": "da8cc3d3233974d7036081b2c4e38b32",
+    "url": "/static/media/stem.da8cc3d3.png"
+  },
+  {
+    "revision": "e3cf2006ec536870e2272302d9137205",
+    "url": "/index.html"
+  }
+];
